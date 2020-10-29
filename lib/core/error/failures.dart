436c42ffe9b45e77af683dbe0c3efc55e8994fb8
unknown <venkat@codinggeek.com>
2020-10-29 13:43:58 +0530

    data sources contracts

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -4,4 +4,10 @@ abstract class Failure extends Equatable {
   // If the subclasses have some properties, they'll get passed to this constructor
   // so that Equatable can perform value comparison.
   Failure([List properties = const <dynamic>[]]);
+
+  List<Object> get props => [];
 }
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
